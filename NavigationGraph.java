92f3ca9c4b8e0212d47635a6bfbe022493ff2481
shitchel <mitch.abts@gmail.com>
2017-05-02 20:02:37 -0500

    Add files via upload

--- NavigationGraph.java
+++ NavigationGraph.java
@@ -1,17 +1,11 @@
-import java.util.List;
-
 public class NavigationGraph implements GraphADT<Location, Path> {
 
 	//TODO: Implement all methods of GraphADT
-	private List<GraphNode<Location, Path>> graph;
-	private String[] edgePropertyNames;
-	
 	
 	public NavigationGraph(String[] edgePropertyNames) {
-		this.edgePropertyNames = edgePropertyNames;
 	}
 
-
+	
 	/**
 	 * Returns a Location object given its name
 	 * 
@@ -20,64 +14,7 @@ public class NavigationGraph implements GraphADT<Location, Path> {
 	 * @return Location object
 	 */
 	public Location getLocationByName(String name) {
-		Location location = new Location(name);
-		return location; //TODO: implement correctly. 
-	}
-
-
-	@Override
-	public void addVertex(Location vertex) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void addEdge(Location src, Location dest, Path edge) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-	@Override
-	public List<Location> getVertices() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public Path getEdgeIfExists(Location src, Location dest) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public List<Path> getOutEdges(Location src) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public List<Location> getNeighbors(Location vertex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public List<Path> getShortestRoute(Location src, Location dest, String edgePropertyName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public String[] getEdgePropertyNames() {
-		// TODO Auto-generated method stub
-		return null;
+		return null; //TODO: implement correctly. 
 	}
 
 }
